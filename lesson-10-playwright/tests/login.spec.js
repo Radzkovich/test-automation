9f8ad3734b3e07fb0b34352b048d6b8d62160c84
Tacciana <123711868+lynxBios@users.noreply.github.com>
2026-06-06 22:07:18 +0300

    test: add successful login test for SauceDemo

---
+++ lesson-10-playwright/tests/login.spec.js
@@ -0,0 +1,31 @@
+// Импортируем 'test' и 'expect' из библиотеки Playwright
+const { test, expect } = require('@playwright/test');
+
+// Описываем наш набор тестов
+test.describe('Авторизация на Sauce Demo', () => {
+
+  // Создаем тест-кейс
+  test('Пользователь должен успешно войти в систему', async ({ page }) => {
+    // 1. Переходим на страницу
+    await page.goto('https://www.saucedemo.com/');
+
+    // 2. Вводим логин
+    // Ищем поле по placeholder, который видит пользователь
+    await page.getByPlaceholder('Username').fill('standard_user');
+
+    // 3. Вводим пароль
+    // Ищем поле по placeholder
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+
+    // 4. Нажимаем кнопку входа
+    // Ищем кнопку по её роли и отображаемому тексту
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // 5. Проверяем, что произошел переход на страницу товаров
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  });
+});
+
+
+
+
