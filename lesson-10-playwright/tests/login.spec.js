71813800c29e330da2447acfc216a6a8f084b096
Tacciana <123711868+lynxBios@users.noreply.github.com>
2026-06-06 22:42:53 +0300

    test: add negative login test for locked out user

--- lesson-10-playwright/tests/login.spec.js
+++ lesson-10-playwright/tests/login.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require('@playwright/test');
 // Описываем наш набор тестов
 test.describe('Авторизация на Sauce Demo', () => {
 
-  // Создаем тест-кейс
+  // Создаем тест-кейс успешного входа
   test('Пользователь должен успешно войти в систему', async ({ page }) => {
     // 1. Переходим на страницу
     await page.goto('https://www.saucedemo.com/');
@@ -24,8 +24,32 @@ test.describe('Авторизация на Sauce Demo', () => {
     // 5. Проверяем, что произошел переход на страницу товаров
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
   });
+
+
+  // Создаем тест-кейс неуспешного входа
+  test('Заблокированный пользователь не должен войти в систему', async ({ page }) => {
+    // 1. Переходим на страницу
+    await page.goto('https://www.saucedemo.com/');
+
+    // 2. Вводим логин
+    // Ищем поле по placeholder, который видит пользователь
+    await page.getByPlaceholder('Username').fill('locked_out_user');
+
+    // 3. Вводим пароль
+    // Ищем поле по placeholder
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+
+    // 4. Нажимаем кнопку входа
+    // Ищем кнопку по её роли и отображаемому тексту
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // 5. Проверяем, что появилось сообщение об ошибке
+    await expect(
+      page.getByText(
+        'Epic sadface: Sorry, this user has been locked out.'
+      )
+    ).toBeVisible();
+    
+  });
+
 });
-
-
-
-
