bdeadd5846b541a8527c4e849a7320eb6fb82328
Tacciana <123711868+lynxBios@users.noreply.github.com>
2026-06-10 21:05:14 +0300

    feat: add CartPage with checkout and navigation actions

---
+++ pages/cart.page.js
@@ -0,0 +1,28 @@
+export class CartPage {
+
+    constructor(page) {
+        this.page = page;
+
+        this.cartItems = page.locator('.cart_item');
+        this.checkoutButton = page.locator('[data-test="checkout"]');
+        this.continueShoppingButton = page.locator('[data-test="continue-shopping"]');
+    }
+
+    async goToCheckout() {
+        await this.checkoutButton.click();
+    }
+
+    async continueShopping() {
+        await this.continueShoppingButton.click();
+    }
+
+    async getCartItemNames() {
+        return await this.page.locator('.inventory_item_name').allTextContents();    
+    }
+
+    async isItemInCart(itemName) {
+        return await this.page.locator('.cart_item')
+            .filter({ hasText: itemName })
+            .count() > 0;
+    }
+}
