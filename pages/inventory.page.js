650e328aa899e9383756de584cc0dfa57cce192c
Tacciana <123711868+lynxBios@users.noreply.github.com>
2026-06-10 20:56:57 +0300

    feat: add InventoryPage with add-to-cart and cart navigation methods

---
+++ pages/inventory.page.js
@@ -0,0 +1,29 @@
+export class InventoryPage {
+
+    constructor(page) {
+        this.page = page;
+
+        this.pageTitle = page.locator('[data-test="title"]');
+        this.sortDropdown = page.locator('[data-test="product-sort-container"]');
+        this.cartButton = page.locator('[data-test="shopping-cart-link"]');
+        this.itemNames = page.locator('.inventory_item_name');
+        this.addToCartButtons = page.locator('button[data-test^="add-to-cart"]');
+    }
+
+    async getPageTitle() {
+        return await this.pageTitle.textContent(); 
+    }  
+    
+
+    async addItemToCart(itemName) {
+        const item = this.page.locator('.inventory_item')
+            .filter({ hasText: itemName });
+
+        await item.locator('button').click();
+
+    }
+
+    async openCart() {
+        await this.cartButton.click();
+    }
+}
